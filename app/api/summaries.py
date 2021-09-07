69e0625b6dd39c76dafc7d4e0d88e6ba0fe04a8e
michael <michael.b@clicktech.com>
2021-09-08 00:38:50 +0100

    Added unit tests and schema models.

--- app/api/summaries.py
+++ app/api/summaries.py
@@ -2,9 +2,10 @@ from fastapi import APIRouter, HTTPException
 
 from app.api import crud
 from app.models.pydantic import SummaryPayloadSchema, SummaryResponseSchema
-
+from app.models.tortoise import SummarySchema
 
 router = APIRouter()
+
 
 @router.post("/", response_model=SummaryResponseSchema, status_code=201)
 async def create_summmary(payload: SummaryPayloadSchema) -> SummaryResponseSchema:
@@ -16,3 +17,17 @@ async def create_summmary(payload: SummaryPayloadSchema) -> SummaryResponseSchem
     }
 
     return response_object
+
+
+@router.get("/{id}/", response_model=SummarySchema)
+async def read_summary(id: int) -> SummarySchema:
+    summary = await crud.get(id)
+    if not summary:
+        raise HTTPException(status_code=404, detail="Summary not found")
+
+    return summary
+
+
+@router.get("/", response_model=list[SummarySchema])
+async def read_all_summaries() -> list[SummarySchema]:
+    return await crud.get_all()
--- app/models/tortoise.py
+++ app/models/tortoise.py
@@ -1,4 +1,5 @@
 from tortoise import fields, models
+from tortoise.contrib.pydantic import pydantic_model_creator
 
 
 class TextSummary(models.Model):
@@ -8,3 +9,7 @@ class TextSummary(models.Model):
 
     def __str__(self):
         return self.url
+
+
+SummarySchema = pydantic_model_creator(TextSummary)
+### def test_read_summary_incorrect_id(test_app_with_db): https://testdriven.io/courses/tdd-fastapi/restful-routes/
