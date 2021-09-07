c17e372d572f3a9446ee03e4fca01c5c30c92870
michael <michael.b@clicktech.com>
2021-09-07 07:19:08 +0100

    Amended ping endpoint to allow test to pass.

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ register_tortoise(
 @app.get("/ping")
 async def pong(settings: Settings = Depends(get_settings)):
     return {
-        "ping": "pong",
+        "ping": "pong!",
         "environment": settings.environment,
         "testing": settings.testing
     }
